f5c7ba98d63a79705b7ead31283b92a110524c2d
agungprayogi16 <agungpurba16@gmail.com>
2024-02-22 11:17:23 +0700

    Finisih

--- CIRCLE-FE-main/src/features/auth/components/Login.tsx
+++ CIRCLE-FE-main/src/features/auth/components/Login.tsx
@@ -1,3 +1,5 @@
+/** @format */
+
 import { Fragment, useEffect, useState } from "react";
 import {
   Alert,
@@ -12,6 +14,7 @@ import {
   InputGroup,
   InputRightElement,
   Text,
+  Image,
 } from "@chakra-ui/react";
 import { Link, useNavigate } from "react-router-dom";
 import { useLogin } from "@/features/auth/hooks/useLogin";
@@ -36,77 +39,118 @@ export default function Login() {
   }, [isLoginSuccess]);
 
   return (
-    <Fragment>
-      <Flex justifyContent={"center"} alignItems={"center"} height={"100vh"}>
-        <Box width={"100%"} maxWidth={"450px"} p={4} color={"white"}>
-          <Heading as="h2" size="3xl" noOfLines={1} color={"green.400"} mb={3}>
-            circle
-          </Heading>
-          <Text fontSize={"xl"} mb={3}>
-            Login to Circle
-          </Text>
-          {isError && (
-            <Alert status="error" bg={"#FF6969"} mb={3} borderRadius={5}>
-              <AlertIcon color={"white"} />
-              <AlertDescription>{error}</AlertDescription>
-            </Alert>
-          )}
-          <FormControl mb={4}>
-            <Input
-              type="text"
-              placeholder="Email/Username *"
-              name="emailOrUsername"
-              value={form.emailOrUsername}
-              onChange={handleChange}
-            />
-          </FormControl>
-          <FormControl mb={4}>
-            <InputGroup size="md">
-              <Input
-                placeholder="Password *"
-                name="password"
-                value={form.password}
-                onChange={handleChange}
-                type={show ? "text" : "password"}
-              />
-              <InputRightElement width="4.5rem">
-                <Button h="1.75rem" size="sm" onClick={() => setShow(!show)}>
-                  {show ? "Hide" : "Show"}
-                </Button>
-              </InputRightElement>
-            </InputGroup>
-          </FormControl>
-          {isLoading ? (
-            <Button
-              isLoading
-              colorScheme="green"
-              variant="solid"
-              borderRadius={"full"}
-              width={"100%"}
-              mb={3}
-            >
-              Loading
-            </Button>
-          ) : (
-            <Button
-              type="submit"
-              borderRadius={"full"}
-              colorScheme="green"
-              width={"100%"}
-              mb={3}
-              onClick={handleLogin}
-            >
-              Login
-            </Button>
-          )}
-          <Text>
-            Have no account yet?{" "}
-            <Link style={{ color: "#48bb78" }} to={"/register"}>
-              Register
-            </Link>
-          </Text>
+    <>
+      <Box
+        width={"full"}
+        display={"flex"}
+        flexDirection={"row"}>
+        <Box flex={1}>
+          <Flex
+            justifyContent={"center"}
+            alignItems={"center"}
+            height={"100vh"}>
+            <Image
+              maxW={"400px"}
+              rounded={"full"}
+              src='./LOGO.png'></Image>
+          </Flex>
         </Box>
-      </Flex>
-    </Fragment>
+        <Box flex={1}>
+          <Fragment>
+            <Flex
+              justifyContent={"center"}
+              alignItems={"center"}
+              height={"100vh"}>
+              <Box
+                width={"100%"}
+                maxWidth={"450px"}
+                p={4}
+                color={"white"}>
+                <Heading
+                  as='h2'
+                  size='3xl'
+                  noOfLines={1}
+                  color={"bclack"}
+                  mb={3}>
+                  CIRCLE
+                </Heading>
+                <Text
+                  fontSize={"xl"}
+                  mb={3}>
+                  Login to Circle
+                </Text>
+                {isError && (
+                  <Alert
+                    status='error'
+                    bg={"#FF6969"}
+                    mb={3}
+                    borderRadius={5}>
+                    <AlertIcon color={"white"} />
+                    <AlertDescription>{error}</AlertDescription>
+                  </Alert>
+                )}
+                <FormControl mb={4}>
+                  <Input
+                    type='text'
+                    placeholder='Email/Username *'
+                    name='emailOrUsername'
+                    value={form.emailOrUsername}
+                    onChange={handleChange}
+                  />
+                </FormControl>
+                <FormControl mb={4}>
+                  <InputGroup size='md'>
+                    <Input
+                      placeholder='Password *'
+                      name='password'
+                      value={form.password}
+                      onChange={handleChange}
+                      type={show ? "text" : "password"}
+                    />
+                    <InputRightElement width='4.5rem'>
+                      <Button
+                        h='1.75rem'
+                        size='sm'
+                        onClick={() => setShow(!show)}>
+                        {show ? "Hide" : "Show"}
+                      </Button>
+                    </InputRightElement>
+                  </InputGroup>
+                </FormControl>
+                {isLoading ? (
+                  <Button
+                    isLoading
+                    colorScheme='green'
+                    variant='solid'
+                    borderRadius={"full"}
+                    width={"100%"}
+                    mb={3}>
+                    Loading
+                  </Button>
+                ) : (
+                  <Button
+                    type='submit'
+                    borderRadius={"full"}
+                    colorScheme='green'
+                    width={"100%"}
+                    mb={3}
+                    onClick={handleLogin}>
+                    Login
+                  </Button>
+                )}
+                <Text>
+                  Have no account yet?{" "}
+                  <Link
+                    style={{ color: "#48bb78" }}
+                    to={"/register"}>
+                    Register
+                  </Link>
+                </Text>
+              </Box>
+            </Flex>
+          </Fragment>
+        </Box>
+      </Box>
+    </>
   );
 }
--- CIRCLE-FE-main/src/features/auth/components/Register.tsx
+++ CIRCLE-FE-main/src/features/auth/components/Register.tsx
@@ -1,3 +1,5 @@
+/** @format */
+
 import { Fragment, useEffect, useState } from "react";
 import {
   Alert,
@@ -12,6 +14,7 @@ import {
   InputGroup,
   InputRightElement,
   Text,
+  Image,
 } from "@chakra-ui/react";
 import { Link, useNavigate } from "react-router-dom";
 import { useRegister } from "@/features/auth/hooks/useRegister";
@@ -36,86 +39,134 @@ export default function Register() {
   }, [isRegisterSuccess]);
 
   return (
-    <Fragment>
-      <Flex justifyContent={"center"} alignItems={"center"} height={"100vh"}>
-        <Box width={"100%"} maxWidth={"450px"} p={4} color={"white"}>
-          <Heading as="h2" size="3xl" noOfLines={1} color={"green.400"} mb={3}>
-            circle
-          </Heading>
-          <Text fontSize={"xl"} mb={3}>
-            Create account Circle
-          </Text>
-          {isError && (
-            <Alert status="error" bg={"#FF6969"} mb={3} borderRadius={5}>
-              <AlertIcon color={"white"} />
-              <AlertDescription>{error}</AlertDescription>
-            </Alert>
-          )}
-          <FormControl mb={4}>
-            <Input
-              type="text"
-              placeholder="Full Name *"
-              name="fullName"
-              value={form.fullName}
-              onChange={handleChange}
-            />
-          </FormControl>
-          <FormControl mb={4}>
-            <Input
-              type="email"
-              placeholder="Email *"
-              name="email"
-              value={form.email}
-              onChange={handleChange}
-            />
-          </FormControl>
-          <FormControl mb={4}>
-            <InputGroup size="md">
-              <Input
-                placeholder="Password *"
-                name="password"
-                value={form.password}
-                type={show ? "text" : "password"}
-                onChange={handleChange}
-              />
-              <InputRightElement width="4.5rem">
-                <Button h="1.75rem" size="sm" onClick={() => setShow(!show)}>
-                  {show ? "Hide" : "Show"}
-                </Button>
-              </InputRightElement>
-            </InputGroup>
-          </FormControl>
-          {isLoading ? (
-            <Button
-              isLoading
-              colorScheme="green"
-              variant="solid"
-              borderRadius={"full"}
-              width={"100%"}
-              mb={3}
-            >
-              Loading
-            </Button>
-          ) : (
-            <Button
-              type="submit"
-              borderRadius={"full"}
-              colorScheme="green"
-              width={"100%"}
-              mb={3}
-              onClick={handleRegister}
-            >
-              Register
-            </Button>
-          )}
-          <Text>
-            Already have an account?{" "}
-            <Link style={{ color: "#48bb78" }} to={"/login"}>
-              Login
-            </Link>
-          </Text>
+    <>
+      <Box
+        width={"full"}
+        display={"flex"}
+        flexDirection={"row"}>
+        <Box flex={1}>
+          <Fragment>
+            <Flex
+              justifyContent={"center"}
+              alignItems={"center"}
+              height={"100vh"}>
+              <Image
+                maxW={"400px"}
+                rounded={"full"}
+                src='./LOGO.png'></Image>
+            </Flex>
+          </Fragment>
         </Box>
-      </Flex>
-    </Fragment>
+        <Box flex={1}>
+          <Fragment>
+            <Flex
+              justifyContent={"center"}
+              alignItems={"center"}
+              height={"100vh"}>
+              <Box
+                width={"100%"}
+                maxWidth={"450px"}
+                p={4}
+                color={"white"}>
+                <Heading
+                  as='h2'
+                  size='3xl'
+                  noOfLines={1}
+                  color={"black"}
+                  mb={3}>
+                  CIRCLE
+                </Heading>
+                <Text
+                  fontSize={"xl"}
+                  mb={3}>
+                  Create account Circle
+                </Text>
+                {isError && (
+                  <Alert
+                    status='error'
+                    bg={"#FF6969"}
+                    mb={3}
+                    borderRadius={5}>
+                    <AlertIcon color={"white"} />
+                    <AlertDescription>{error}</AlertDescription>
+                  </Alert>
+                )}
+                <FormControl mb={4}>
+                  <Input
+                    type='text'
+                    placeholder='Full Name *'
+                    name='fullName'
+                    value={form.fullName}
+                    onChange={handleChange}
+                  />
+                </FormControl>
+                <FormControl mb={4}>
+                  <Input
+                    type='email'
+                    placeholder='Email *'
+                    name='email'
+                    value={form.email}
+                    onChange={handleChange}
+                  />
+                </FormControl>
+                <FormControl mb={4}>
+                  <InputGroup size='md'>
+                    <Input
+                      placeholder='Password *'
+                      name='password'
+                      value={form.password}
+                      type={show ? "text" : "password"}
+                      onChange={handleChange}
+                    />
+                    <InputRightElement width='4.5rem'>
+                      <Button
+                        h='1.75rem'
+                        size='sm'
+                        onClick={() => setShow(!show)}>
+                        {show ? "Hide" : "Show"}
+                      </Button>
+                    </InputRightElement>
+                  </InputGroup>
+                </FormControl>
+                {isLoading ? (
+                  <Button
+                    isLoading
+                    colorScheme='green.500'
+                    variant='solid'
+                    borderRadius={"full"}
+                    width={"100%"}
+                    mb={3}>
+                    Loading
+                  </Button>
+                ) : (
+                  <Button
+                    type='submit'
+                    borderRadius={"full"}
+                    backgroundColor='black'
+                    color={"white"}
+                    width={"100%"}
+                    mb={3}
+                    onClick={handleRegister}>
+                    Register
+                  </Button>
+                )}
+                <Text>
+                  Already have an account?{" "}
+                  <Link
+                    style={{
+                      color: "green",
+                      cursor: "pointer",
+                      textDecoration: "underline",
+                    }}
+                    to={"/login"}>
+                    Login
+                  </Link>
+                </Text>
+              </Box>
+            </Flex>
+          </Fragment>
+        </Box>
+      </Box>
+    </>
   );
 }
